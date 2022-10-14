1f4f1c03cda9aaca0c8d0ddd13ea515fb288e98e
Findlay20 <cameron.findlay@hotmail.co.uk>
2022-10-14 12:38:01 +0100

    Updated handlers.js

--- handlers.js
+++ handlers.js
@@ -1,10 +1,10 @@
 const fs = require('fs');
 const { messageParent } = require('jest-worker');
 const path = require('path');
+
 const html = fs.readFileSync(path.resolve(__dirname, 'index.html'), 'utf-8');
 
-let submit = document.querySelector('#submit')  //set id in index.html
-
+let submit = document.querySelector('#submit')
 submit.addEventListener('click', generateCard)
 
 
